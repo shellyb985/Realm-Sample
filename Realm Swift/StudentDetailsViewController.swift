dcb73db7c3279a82bb3dd53fe8cc96d52bdd066b
Shelly Pritchard <shelly.pritchard@merucabs.com>
2017-12-14 17:51:22 +0530

    folder structure added

---
+++ Realm Swift/StudentDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StudentDetailsViewController.swift
+//  Realm Swift
+//
+//  Created by Administrator on 14/12/17.
+//  Copyright © 2017 spb. All rights reserved.
+//
+
+import UIKit
+
+class StudentDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
